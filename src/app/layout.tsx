c673db185f4f2bdfd2a4e66e8997616a2ba49b3e
rahulkappad <154733085+rahulkappad@users.noreply.github.com>
2025-04-04 17:59:47 +0300

    move

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import {Inter} from "next/font/google";
-import Header from "./components/layout/Header";
+import Header from "@/components/layout/Header";
+
 
 const inter = Inter({subsets:["latin"] });
 
