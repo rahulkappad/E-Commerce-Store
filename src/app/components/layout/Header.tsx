b85fd31f0760b48bb5320c2a6a9a9988a1ecc338
rahulkappad <154733085+rahulkappad@users.noreply.github.com>
2025-04-04 12:07:07 +0300

    initil stage

---
+++ src/app/components/layout/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const AnnoucementBar = () => {
+    return(
+        <div className='w-full bg-black py-2'>
+            <div className='container mx-auto flex'>
+                <span>
+
+                </span>
+            </div>
+        </div>
+    )
+}
+
+const Header = () => {
+  return (
+    <div>
+      Header
+    </div>
+  )
+}
+
+export default Header
